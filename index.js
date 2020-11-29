edd4b4b04b454d3e0ee974353709f232591e8f06
Jose Campos <josecamposh95@gmail.com>
2020-11-29 15:00:17 -0300

    solucion

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const drawApp = () => {
     </div>
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    </div>
-  `
+    </div> `
 };
 
 const getPokemones = async () => {
@@ -30,7 +29,6 @@ const getPokemones = async () => {
     console.log(error);
   }
 };
-
 
 drawApp();
 getPokemones();
@@ -62,11 +60,18 @@ const crearLista = () => {
 }
 
 const crearPokemonCard = (name, stats, imageSrc) => {
-  misPokemones.push({
-    name,
-    imageSrc,
-    stats
-  });
+  const pokemonSeleccionado = misPokemones.find(pokemon => pokemon.name === name);
+
+  if (pokemonSeleccionado) {
+    pokemonSeleccionado.quantity++;
+  } else {
+    misPokemones.push({
+      name,
+      imageSrc,
+      stats,
+      quantity: 1
+    });
+  }
 
   misPokemonesCards();
 }
@@ -83,6 +88,7 @@ const pokemonCard = (pokemon, i) => {
       <div class="card">
         <div class="card-body">
           <h4 class="text-center">${pokemon.name}</h4>
+          <h4 class="text-center">Cantidad: ${pokemon.quantity}</h4>
           <div class="d-flex flex-wrap align-items-center">
             <div class="col-sm-12 col-md-6">
               <img class="card-img-top" style="max-height: 200px; max-width: 200px;" src="${pokemon.imageSrc}" alt="${pokemon.name}">
@@ -91,6 +97,9 @@ const pokemonCard = (pokemon, i) => {
               <div class="card-text">
                 ${ crearStats ( pokemon.stats ) }
                 <div class="d-flex justify-content-center">
+                  <button type="button" class="btn btn-primary mr-2" data-toggle="modal" data-target="#exampleModal" onclick="editPokemon(${i})">
+                    Editar
+                  </button>
                   <button class="btn btn-danger" onclick="deleteCard(${i})">Eliminar</button>
                 </div>
               </div>
@@ -107,6 +116,54 @@ const crearStats = (stats) => {
 }
 
 const deleteCard = (index) => {
-  misPokemones.splice(index, 1);
+
+  if (misPokemones[index].quantity > 1) {
+    misPokemones[index].quantity--;
+  } else {
+    misPokemones.splice(index, 1);
+  }
+
+  misPokemonesCards();
+}
+
+const editPokemon = (index) => {
+  const modal = document.getElementById("exampleModal");
+  modal.innerHTML = `
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Editar Pokemon</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h5>${ misPokemones[index].name }</h5>
+        <h5>Cantidad: ${ misPokemones[index].quantity }</h5>
+        <img src="${ misPokemones[index].imageSrc }" alt="${misPokemones[index].name}" />
+        <div class="d-flex flex-column">
+          <label for="quantity">Nueva cantidad</label>
+          <input type="number" id="quantity" name="quantity" value="${misPokemones[index].quantity}">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button id="botonCerrar" type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary" onclick="guardarCambios(${index})">Guardar cambios</button>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
+const guardarCambios = (index) => {
+  const quantity = document.getElementById("quantity").value;
+  
+  if (quantity <= 0) {
+    alert("Ingrese un valor mayor o igual a 1");
+  } else {
+    misPokemones[index].quantity = quantity;
+    document.getElementById("botonCerrar").click();
+  }
+
   misPokemonesCards();
 }
